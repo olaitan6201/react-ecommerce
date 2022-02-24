c1796c3a6b9f6e1cdc5f15b2663526112b56382f
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-02-24 17:59:38 +0100

    Adding header component with links, logo and stylings

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 
 import { HomePage } from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop/shop.component';
+import { Header } from './components/header/header.component';
 
 
 function App() {
-  return <div>
-    {/* <HomePage /> */}
-    <Routes>
-      <Route path='' element={<HomePage />}/>
-      <Route path='/shop' element={<ShopPage />}/>
-    </Routes>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path='' element={<HomePage />}/>
+        <Route path='/shop' element={<ShopPage />}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,15 @@
+import './header.styles.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+export const Header = () => (
+    <div className="header">
+        <Link to="/" className='logo-container'>
+            <Logo className='logo'/>
+        </Link>
+        <div className="options">
+            <Link className="option" to='/shop'>SHOP</Link>
+            <Link className="option" to='/shop'>CONTACT</Link>
+        </div>
+    </div>
+);
