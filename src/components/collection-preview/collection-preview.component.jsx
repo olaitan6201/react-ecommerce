933d4c1fd3c9c4daca53a84bc45e59d95248125f
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-02-24 17:48:11 +0100

    Created Collection item with stylings update

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,3 +1,4 @@
+import { CollectionItem } from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 export const CollectionPreview = ({title, items}) => (
@@ -7,8 +8,8 @@ export const CollectionPreview = ({title, items}) => (
             {
                 items
                 .filter((item, idx) => idx < 4)
-                .map(item => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps}/>
                 ))
             }
         </div>
