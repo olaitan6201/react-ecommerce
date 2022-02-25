e43302c03fe6b98e8c882a20e46e196b3dc1477b
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-02-25 21:46:09 +0100

    Created form input component

---
+++ src/components/forms/form-input/form-input.component.jsx
@@ -0,0 +1,21 @@
+import './form-input.styles.scss';
+
+export const FormInput = ({handleChange, label, ...otherProps}) => (
+    <div className="form-group">
+        <input 
+            className="form-input" 
+            onChange={handleChange}
+            {...otherProps}
+        />
+        {
+            label ? (
+                <label 
+                    className={
+                        `${otherProps.value.length?'shrink':''} 
+                        form-input-label`
+                    }
+                >{label}</label>
+            ) : null
+        }
+    </div>
+);
