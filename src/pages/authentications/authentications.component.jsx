5317a21246c5c971edb604ac844ac2997fc2669c
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-02-25 21:45:25 +0100

    Added authentications component for sign in and sign up page

---
+++ src/pages/authentications/authentications.component.jsx
@@ -0,0 +1,8 @@
+import { SignIn } from '../../components/authetications/sign-in/sign-in.component';
+import './authentications.styles.scss';
+
+export const Authentications = () => (
+    <div className="authentications">
+        <SignIn />
+    </div>
+);
