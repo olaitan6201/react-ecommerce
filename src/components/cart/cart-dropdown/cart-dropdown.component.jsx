da51c9986653b422da3561d2502296192d746ec7
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-03-14 00:34:05 +0100

    Implemented cart empty message

--- src/components/cart/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart/cart-dropdown/cart-dropdown.component.jsx
@@ -12,9 +12,14 @@ const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
-                cartItems.map(
-                    cartItem => <CartItem key={cartItem.id} item={cartItem}/>
-                )
+                cartItems.length ?
+                    cartItems.map(
+                        cartItem => <CartItem key={cartItem.id} item={cartItem}/>
+                    )
+                :
+                <div className="empty-message">
+                    Your cart is empty
+                </div>
             }
         </div>
         <CustomButton>Go To Checkout</CustomButton>
