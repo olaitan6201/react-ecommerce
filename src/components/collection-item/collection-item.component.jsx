121b636e5cc655133da8965bd3e030ac19bc1e85
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-03-13 21:49:04 +0100

    Added Cart Item styling with CustomButton

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import './collection-item.styles.scss';
 
+import { CustomButton } from '../forms/custom-button/custom-button.component';
+
 export const CollectionItem = ({id, name, price, imageUrl}) => (
     <div className="collection-item">
         <div 
@@ -12,5 +14,6 @@ export const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className="name">{name}</span>
             <span className="price">${price}</span>
         </div>
+        <CustomButton inverted>Add to Cart</CustomButton>
     </div>
 );
--- src/components/forms/custom-button/custom-button.component.jsx
+++ src/components/forms/custom-button/custom-button.component.jsx
@@ -1,8 +1,12 @@
 import './custom-button.styles.scss';
 
-export const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
+export const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
     <button 
-        className={`${isGoogleSignIn? 'google-sign-in':''} custom-button`}
+        className={`
+            ${isGoogleSignIn? 'google-sign-in':''} 
+            ${inverted? 'inverted':''} 
+            custom-button`
+        }
         {...otherProps}
     >
         {children}
