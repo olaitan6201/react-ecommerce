024a757c698758207cf7405497fb90da6278f5d0
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-03-18 00:37:51 +0100

    Data Normalization (Converting Shop Data array to object and passing selector of an array)

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,7 +1,7 @@
 import './collections-overview.styles.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectShopItems } from '../../redux/shop/shop.selectors';
+import { selectCollectionsArray } from '../../redux/shop/shop.selectors';
 import { CollectionPreview } from '../collection-preview/collection-preview.component';
 
 const CollectionsOverview = ({collections}) => (
@@ -16,7 +16,7 @@ const CollectionsOverview = ({collections}) => (
 
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopItems
+    collections: selectCollectionsArray
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,25 +1,31 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3, 
-    womens: 4,
-    mens: 5
-}
+// const COLLECTION_ID_MAP = {
+//     hats: 1,
+//     sneakers: 2,
+//     jackets: 3, 
+//     womens: 4,
+//     mens: 5
+// }
 
 const selectShop = state => state.shop;
 
-export const selectShopItems = createSelector(
+export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+);
+
+export const selectCollectionsArray = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
 )
 
 
 export const selectCollection = collectionParam => 
     createSelector(
-        [selectShopItems],
-        collections => collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionParam]
-        )
+        [selectCollections],
+        collections => collections[collectionParam]
     );
+// collections => collections.find(
+//     collection => collection.id === COLLECTION_ID_MAP[collectionParam]
+// )
